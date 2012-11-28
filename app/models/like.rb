59c7668b48eb40ae173a3ad67cbccc69e8206c45
JM <james@ubuntu.(none)>
2012-11-27 20:39:21 -0800

    Database works if you run bundle install and then rake db:setup

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,5 @@
 class Like < ActiveRecord::Base
   # new columns need to be added here to be writable through mass assignment
-  #attr_accessible :username, :email,:password, :password_confirmation
   attr_accessible :user_id, :photo_id
 
   validates_presence_of :user_id
