5e3c655593942e91b4c1c2a79a75a3f7db1ec99c
JM <james@ubuntu.(none)>
2012-11-29 19:58:03 -0800

    Got logout working

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -9,6 +9,11 @@ class LoginsController < ActionController::Base
 
   def index
     @login_fail = false
+    @action = params[:user_auth_action]
+    if @action == "logout"
+      session[:current_user_id] = nil
+      return render :action => "index"
+    end
   end
 
   def new
@@ -16,23 +21,24 @@ class LoginsController < ActionController::Base
   end
 
   def create
-    if user = User.authenticate(params[:username], params[:password])
-      session[:current_user_id] = user.id
-      redirect_to photos_url
-    else
-      if params[:signup] == "signup"
-        if user = User.create_new_user(params)
-          session[:current_user_id] = user.id
-          redirect_to photos_url
-        else
-          @signup_fail = true
-          @error_message = "Someone with the same username already exists. Choose a new one."
-          return render :action => "index"
-        end
-        #Do signup stuff
+    @action = params[:user_auth_action]
+    session[:received_arg] = @action
+    if @action == "login"
+      if user = User.authenticate(params[:username], params[:password])
+        session[:current_user_id] = user.id
+        redirect_to photos_url
       else
         @login_fail = true
         @error_message = "The given login was not found or the password did not match."
+        return render :action => "index"
+      end
+    elsif @action == "signup"
+      if user = User.create_new_user(params)
+        session[:current_user_id] = user.id
+        redirect_to photos_url
+      else
+        @signup_fail = true
+        @error_message = "Someone with the same username already exists. Choose a new one."
         return render :action => "index"
       end
     end
