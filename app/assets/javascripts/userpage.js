8848eef3e3c922cc14c527e376642f731e0579f4
JM <james@ubuntu.(none)>
2012-12-04 13:36:28 -0800

    Added user page and fixed the description a little bit.

--- app/assets/javascripts/userpage.js
+++ app/assets/javascripts/userpage.js
@@ -2,10 +2,14 @@ $(function() {
     $("#showbutton").click(function() {
         expanded = $(".user-bio").css("height") !== "0px";
         if (expanded) {
-            $(this).html("show")
+            $(this).html("show");
+            $(".user-bio").css("padding-top", "0px");
+            $(".user-bio").css("padding-bottom", "0px");
         }
         else {
-            $(this).html("hide")
+            $(this).html("hide");
+            $(".user-bio").css("padding-top", "10px");
+            $(".user-bio").css("padding-bottom", "10px");
         }
         $(".user-bio").animate({
             height: (expanded ? "0px" : "100px")
---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,41 @@
+class UserController < ApplicationController
+  protect_from_forgery
+
+  def show
+  end
+
+  def index
+    get_and_store_username()
+    if !@user
+    end
+  end
+
+  def new
+  end
+
+  def create
+    #get_and_store_username()
+    #flash[:notice] = "success upload"
+    #@photo = Photo.new(params[:photo])
+    #@photo.user = @user
+    #@photo.save
+    #redirect_to photos_path
+    #return
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def get_and_store_username
+    @user = User.find_by_id(session[:current_user_id])
+    puts session[:current_user_id]
+    puts User.find_by_id(session[:current_user_id])
+  end
+
+end
