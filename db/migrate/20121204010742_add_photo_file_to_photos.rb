13187e7b0b87bec9a0eb67b6485a6fabd7cfd9ae
JM <james@ubuntu.(none)>
2012-12-05 04:30:26 -0800

    Fixed uploading. Naming was wrong in migration

--- db/migrate/20121204010742_add_photo_file_to_photos.rb
+++ db/migrate/20121204010742_add_photo_file_to_photos.rb
@@ -1,14 +1,14 @@
 class AddPhotoFileToPhotos < ActiveRecord::Migration
   def self.up
-    add_column :photos, :photo_file_file_name, :string
-    add_column :photos, :photo_file_content_type, :string
-    add_column :photos, :photo_file_file_size, :integer
-    add_column :photos, :photo_file_updated_at, :datetime
+    add_column :photos, :photo_file_name, :string
+    add_column :photos, :photo_content_type, :string
+    add_column :photos, :photo_file_size, :integer
+    add_column :photos, :photo_updated_at, :datetime
   end
   def self.down
-    remove_column :photos, :photo_file_file_name
-    remove_column :photos, :photo_file_content_type
-    remove_column :photos, :photo_file_file_size
-    remove_column :photos, :photo_file_updated_at
+    remove_column :photos, :photo_file_name
+    remove_column :photos, :photo_content_type
+    remove_column :photos, :photo_file_size
+    remove_column :photos, :photo_updated_at
   end
 end
