0c33543941d0009078a229ff0795ce9f270c685d
JM <james@ubuntu.(none)>
2012-12-06 10:16:12 -0800

    Updated user page as well, and moved some of the scripts around

--- app/assets/javascripts/pic.js
+++ app/assets/javascripts/pic.js
@@ -1,6 +1,6 @@
 $(function() {
-  t=150;
-  $(".pic-thumb-container").hover(
+    t=150;
+    $(".pic-thumb-container").hover(
     function() {
       $(this).children(".pic-thumb-descr").stop().animate({
         opacity: '1'
@@ -17,19 +17,27 @@ $(function() {
         opacity: '0'
       },t);
     });
-  $(".filters-item").click(function() {
+    $(".filters-item").click(function() {
     $("#viewing").text($(this).text());
-  });
+    });
 
-  $(".black-background-specific").click(function() {
+    $(".black-background-specific").click(function() {
       $("#photo-popup-view").css("z-index", -10);
       $("#map-popup-view").css("z-index", -10);
       $("#photo-popup-view").css("visibility", "hidden");
       $("#map-popup-view").css("visibility", "hidden");
-  });
+    });
+    $("#go-to-map-view").click(function() {
+        $("#map-popup-view").css("z-index", 10);
+        $("#photo-popup-view").css("z-index", -10);
+        $("#map-popup-view").css("visibility", "visible");
+        $("#photo-popup-view").css("visibility", "hidden");
+    });
+    $("#go-to-photo-view").click(function() {
+        $("#map-popup-view").css("z-index", -10);
+        $("#photo-popup-view").css("z-index", 10);
+        $("#map-popup-view").css("visibility", "hidden");
+        $("#photo-popup-view").css("visibility", "visible");
+    });
 
-	$("#photo-popup-view a.btn-mini").click(function() {
-		newtext = $(this).siblings("input[type='text']").val();
-		$(".popup-comment-box").append('<div class="popup-comment"><span class="popup-comment-username"><a>amadeus</a>: </span><span class="popup-comment-text">'+newtext+'</span></div>');
-	});
 });
