11fb0e5c4ae77255386a633cc2cd91fca9a5f316
JM <james@ubuntu.(none)>
2012-12-06 12:10:57 -0800

    User links go to the correct user's page

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,11 +2,16 @@ class UserController < ApplicationController
   protect_from_forgery
 
   def show
+    get_and_store_username()
+    @pages_user = User.find_by_username(params[:id])
+    get_users_photos(@pages_user.id)
+    return render :action => "index"
   end
 
   def index
     get_and_store_username()
-    get_users_photos(1)
+    @pages_user = @user
+    get_users_photos(@user.id)
     if params[:success] != nil
       @display_status = true;
       #@status = "Uploaded photo successfully";
@@ -40,8 +45,8 @@ class UserController < ApplicationController
     end
   end
 
-  def get_users_photos(page)
-    @photos = Photo.order("created_at DESC").where(:user_id => @user.id)
+  def get_users_photos(id)
+    @photos = Photo.order("created_at DESC").where(:user_id => id)
     if !@photos.first
       @photo_error = "No uploaded photos."
     end
